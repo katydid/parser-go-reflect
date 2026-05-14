a0baf274f68fb52f3a56e63aef6bc0e1b74b8a9b
Walter Schulze <awalterschulze@gmail.com>
2026-05-14 09:25:39 +0100

    do not unnecessary set the p.state.value to speed up 5% more

--- reflect/parse/state.go
+++ reflect/parse/state.go
@@ -64,33 +64,33 @@ func (p *parser) newState(val reflect.Value) {
 		p.state.kind = enterStructState
 		p.state.fieldKind = structKind
 		p.state.parent = value
-		p.state.value = reflect.Value{}
+		// p.state.value = reflect.Value{}
 		p.state.field = -1
 		p.state.maxField = value.NumField()
-		p.state.mapIter = nil
+		// p.state.mapIter = nil
 	} else if isSlice(value) {
 		p.state.kind = enterSliceState
 		p.state.fieldKind = sliceKind
 		p.state.parent = value
-		p.state.value = reflect.Value{}
+		// p.state.value = reflect.Value{}
 		p.state.field = -1
 		p.state.maxField = value.Len()
-		p.state.mapIter = nil
+		// p.state.mapIter = nil
 	} else if value.Kind() == reflect.Map {
 		p.state.kind = enterMapState
 		p.state.fieldKind = mapKind
 		p.state.parent = value
-		p.state.value = reflect.Value{}
+		// p.state.value = reflect.Value{}
 		p.state.field = -1
 		p.state.maxField = value.Len()
 		p.state.mapIter = value.MapRange()
 	} else {
 		p.state.kind = valueState
 		p.state.fieldKind = valueKind
-		p.state.parent = reflect.Value{}
+		// p.state.parent = reflect.Value{}
 		p.state.value = val
 		p.state.field = -1
 		p.state.maxField = 1
-		p.state.mapIter = nil
+		// p.state.mapIter = nil
 	}
 }
